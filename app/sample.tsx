a25fd143c4563a8c11d3b5ff6b88169aaba00917
Robbie <robbiethewadley@gmail.com>
2025-02-13 15:49:09 -0700

    cleanup

--- app/sample.tsx
+++ app/sample.tsx
@@ -1,8 +1,7 @@
 export default function Sample() {
 	return (
 		<div>
-			<div>This is sample.tsx, when you edit me I should hot reload but I don't.</div>
-			<div>give it a try</div>
+			This is sample.tsx, when you edit me I should hot reload but I don't.
 		</div>
 	);
 }
