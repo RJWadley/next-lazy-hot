530186c9f07b9b5971e51421203793318e408dec
Robbie <robbiethewadley@gmail.com>
2025-02-13 15:49:50 -0700

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Sample = lazy(() => import("./sample"));
 export default function Home() {
 	return (
 		<div>
-			This is the main page. Hot reloading works fine here
+			This is the main page. Hot reloading works fine here.
 			<Sample />
 		</div>
 	);
